96654bca306a402a1ffa245c616de0dd7146aa3b
DanieleRomani <124621366+DanieleRomani@users.noreply.github.com>
2023-03-27 17:32:31 +0200

    Exe_34_Advanced_IDE_Features_Debugger

--- src/it/Exercise_Complete/Checkpoint/Main.java
+++ src/it/Exercise_Complete/Checkpoint/Main.java
@@ -1,44 +1,44 @@
 package it.Exercise_Complete.Checkpoint;
 
-import it.Exercise_Complete.Exe_Rip_4.Auto;
-
-public class Main {
-    static CarrelloSpesa carrello = new CarrelloSpesa();
-
-    /**
-     * Istanziare 3 oggetti di tipo Prodotto
-     */
-    public static void main(String[] args) {
-        Prodotto p101 = new Prodotto("101", "Pane", 1.20);
-        Prodotto p102 = new Prodotto("102", "mele", 0.50);
-        Prodotto p103 = new Prodotto("103", "Sale", 0.89);
-        /**
-         * Istanziare un oggetto CarrelloSpesa e aggiungere i prodotti creati
-         */
-        carrello.aggiungiProdotto(101);
-        carrello.aggiungiProdotto(102);
-        carrello.aggiungiProdotto(103);
-
-        System.out.println("Prodotti nel Carrello: ");
-        carrello.stampaProdotti();
-        /**
-         * Stampare il numero di prodotti totali presenti nel carrello
-         */
-        System.out.println("Numero dei prodotti presenti nel carrello" + carrello.getNumeroProdotti() + "N");
-        /**
-         * Stampare il prezzo totale dei prodotti nel carrello
-         */
-        System.out.println("Prezzo totale: " + carrello.getPrezzoTotale() + " euro");
-
-        /**
-         * Creare un nuovo prodotto e aggiungerlo al carrello.
-         */
-        Prodotto p104 = new Prodotto("104", "Zucchero", 0.50);
-        carrello.aggiungiProdotto(p104);
-        carrello.rimuoviProdotto("103");
-
-        carrello.stampaProdotti();
-
-        System.out.println("Numero dei prodotti presenti nel carrello" + carrello.getNumeroProdotti() + "N")
-    }
-}
+//import it.Exercise_Complete.Exe_Rip_4.Auto;
+//
+//public class Main {
+//    static CarrelloSpesa carrello = new CarrelloSpesa();
+//
+//    /**
+//     * Istanziare 3 oggetti di tipo Prodotto
+//     */
+//    public static void main(String[] args) {
+//        Prodotto p101 = new Prodotto("101", "Pane", 1.20);
+//        Prodotto p102 = new Prodotto("102", "mele", 0.50);
+//        Prodotto p103 = new Prodotto("103", "Sale", 0.89);
+//        /**
+//         * Istanziare un oggetto CarrelloSpesa e aggiungere i prodotti creati
+//         */
+//        carrello.aggiungiProdotto(101);
+//        carrello.aggiungiProdotto(102);
+//        carrello.aggiungiProdotto(103);
+//
+//        System.out.println("Prodotti nel Carrello: ");
+//        carrello.stampaProdotti();
+//        /**
+//         * Stampare il numero di prodotti totali presenti nel carrello
+//         */
+//        System.out.println("Numero dei prodotti presenti nel carrello" + carrello.getNumeroProdotti() + "N");
+//        /**
+//         * Stampare il prezzo totale dei prodotti nel carrello
+//         */
+//        System.out.println("Prezzo totale: " + carrello.getPrezzoTotale() + " euro");
+//
+//        /**
+//         * Creare un nuovo prodotto e aggiungerlo al carrello.
+//         */
+//        Prodotto p104 = new Prodotto("104", "Zucchero", 0.50);
+//        carrello.aggiungiProdotto(p104);
+//        carrello.rimuoviProdotto("103");
+//
+//        carrello.stampaProdotti();
+//
+//        System.out.println("Numero dei prodotti presenti nel carrello" + carrello.getNumeroProdotti() + "N")
+//    }
+//}
--- src/it/Exercise_Complete/Exe_Rip_JAdvanced/Articolo.java
+++ src/it/Exercise_Complete/Exe_Rip_JAdvanced/Articolo.java
@@ -1,40 +1,40 @@
 package it.Exercise_Complete.Exe_Rip_JAdvanced;
-
-public class Articolo extends Product implements Sconto {
-    private int id;
-    private String descrizione;
-    private TipoProdotto tipoProdotto;
-    private double sconto = 0.0;
-
-    public Articolo(int id, String nome, double prezzo, String descrizione, TipoProdotto tipoProdotto) {
-        super(nome, prezzo);
-        this.id = id;
-        this.descrizione = descrizione;
-        this.tipoProdotto = tipoProdotto;
-    }
-
-    @Override
-    public void applicaSconto(double sconto) {
-        this.sconto = sconto;
-    }
-
-    @Override
-    public double calcolaPrezzoScontato() {
-        double prezzo = getPr
-        ezzo();
-        double sconto = this.sconto / 100.0;
-        return prezzo - (prezzo * sconto);
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getDescrizione() {
-        return descrizione;
-    }
-
-    public TipoProdotto getTipoProdotto() {
-        return tipoProdotto;
-    }
-}
+//
+//public class Articolo extends Product implements Sconto {
+//    private int id;
+//    private String descrizione;
+//    private TipoProdotto tipoProdotto;
+//    private double sconto = 0.0;
+//
+//    public Articolo(int id, String nome, double prezzo, String descrizione, TipoProdotto tipoProdotto) {
+//        super(nome, prezzo);
+//        this.id = id;
+//        this.descrizione = descrizione;
+//        this.tipoProdotto = tipoProdotto;
+//    }
+//
+//    @Override
+//    public void applicaSconto(double sconto) {
+//        this.sconto = sconto;
+//    }
+//
+//    @Override
+//    public double calcolaPrezzoScontato() {
+//        double prezzo = getPr
+//        ezzo();
+//        double sconto = this.sconto / 100.0;
+//        return prezzo - (prezzo * sconto);
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public String getDescrizione() {
+//        return descrizione;
+//    }
+//
+//    public TipoProdotto getTipoProdotto() {
+//        return tipoProdotto;
+//    }
+//}
